e0925d11af18fd69a3b6e473e65bb475ff596e3f
Jose Carrasco <jcarrasko@gmail.com>
2016-05-02 01:47:28 +0200

    adding libraries for offline use

---
+++ www/js/service/updateService.js
@@ -0,0 +1,45 @@
+document.addEventListener("deviceready", init, false);
+
+//The directory to store data
+var store;
+
+//Used for status updates
+var $status;
+
+//URL of our asset
+var assetURL = "http://www.furgovw.org/api.php...";
+
+//File name of our important data file we didn't ship with the app
+var fileName = "mydatafile.txt";
+
+function init() {
+    
+    $status = document.querySelector("#status");
+
+    $status.innerHTML = "Checking for data file.";
+
+    store = cordova.file.dataDirectory;
+
+    //Check for the file. 
+    window.resolveLocalFileSystemURL(store + fileName, appStart, downloadAsset);
+
+}
+
+function downloadAsset() {
+    var fileTransfer = new FileTransfer();
+    console.log("About to start transfer");
+    fileTransfer.download(assetURL, store + fileName, 
+        function(entry) {
+            console.log("Success!");
+            appStart();
+        }, 
+        function(err) {
+            console.log("Error");
+            console.dir(err);
+        });
+}
+
+//I'm only called when the file exists or has been downloaded.
+function appStart() {
+    $status.innerHTML = "App ready!";
+}
